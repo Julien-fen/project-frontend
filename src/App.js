74612a991b26e9016ea64dfd596e9444cde1e93c
Julien-fen <julien.fenelon@gmail.com>
2024-11-27 19:05:41 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ClassList from './components/ClassList';
+
+function App() {
+  return (
+    <div>
+      <h1>Gestion des Classes</h1>
+      <ClassList />
+    </div>
+  );
+}
+
+export default App;
