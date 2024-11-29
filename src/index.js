0b835996be1efc78b6c5c85c4430596446f51cd9
Julien-fen <julien.fenelon@gmail.com>
2024-11-28 20:31:13 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css'; // Vous pouvez commenter cette ligne si ce fichier n'existe pas
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
